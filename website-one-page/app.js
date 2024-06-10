034f58ea857d3a9c43f08c19d369ed47d43ceea6
anacampaner <ana.fsilva@sptech.school>
2024-06-10 18:22:04 -0300

    npm Consertado

--- website-one-page/app.js
+++ website-one-page/app.js
@@ -17,11 +17,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
-var propriedadeRouter = require("./src/routes/propriedades"); // Importa o roteador de propriedades
+var propriedadeRouter = require("./src/routes/propriedade"); // Importa o roteador de propriedades
 
 
 app.use(express.json());
@@ -32,11 +28,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
-app.use("/propriedades", propriedadeRouter); // Adiciona a rota de propriedades
+app.use("/propriedade", propriedadeRouter); // Adiciona a rota de propriedades
 
 
 app.listen(PORTA_APP, function () {
--- website-one-page/src/models/propriedadeModel.js
+++ website-one-page/src/models/propriedadeModel.js
@@ -2,25 +2,35 @@ var database = require("../database/config");
 
 
 //Cadastrar dados do silo no banco de dados
-function cadastrar(logradouro,numero,cep,nome)
+function cadastrar(logradouro, numero, cep, nome, tempMin, tempMax, umiMin, umiMax) 
 {
-    
-    var instrucaoSql1 = `INSERT INTO endereco(logradouro,numero,cep) VALUES ('${logradouro}','${numero}','${cep}')`;
+    // Primeiro INSERT: insere o endereço
+    var instrucaoSql1 = `INSERT INTO endereco (logradouro, numero, cep) VALUES ('${logradouro}', '${numero}', '${cep}');`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql1);
 
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucaoSql2 = 
-    `
-        INSERT INTO propriedade(nome,proprietario,fkEndereco,fkUsuario) VALUES ('${nome},'Ana',2,1);
-    `;
+    // Executa o primeiro INSERT e obtém o ID do endereço inserido
+    database.executar(instrucaoSql1, function (resultado1) 
+    {
+        var fkEndereco = resultado1.insertId;  // Obtém o ID do endereço inserido
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql2);
-    return database.executar(instrucaoSql2,instrucaoSql1);
+        // Segundo INSERT: insere a propriedade com o ID do endereço recém-inserido
+        var instrucaoSql2 = `INSERT INTO propriedade (nome, proprietario, fkEndereco, fkUsuario) VALUES ('${nome}', 'Ana', ${fkEndereco}, 1);`;
+
+        console.log("Executando a instrução SQL: \n" + instrucaoSql2);
+
+        // Executa o segundo INSERT
+        database.executar(instrucaoSql2, function (resultado2) 
+        {
+            // Aqui você pode manipular o resultado da segunda inserção, se necessário
+            console.log("Propriedade cadastrada com sucesso!");
+        });
+    });
 }
 
-function buscarPropriedadesPorUsuario(idUsuario) {
+
+function buscarPropriedadesPorUsuario(idUsuario) 
+{
   var instrucaoSql = `SELECT * FROM propriedade WHERE fkUsuario = '${idUsuario}'`;
   return database.executar(instrucaoSql);
 }
